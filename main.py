2b3b1b1395263e1671a6956ef9435428eeadd066
Denysmak <denysmakene13@gmail.com>
2022-07-23 18:06:01 -0300

    muda título, icone e cor do background

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pygame
 
-# Initialize the pygame
+# INITIALIZE THE PYGAME
 pygame.init()
 
-#create the screen
+#CREATE THE SCREEN
 screen = pygame.display.set_mode((800, 600))
 
+#TITLE AND ICON
+#isso muda o título que aparece no pygame
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('spaceship.png')
+#isso coloca a variável icon como icone do pygame
+pygame.display.set_icon(icon)
 
-
+#GAME LOOP
 running = True
 #enquanto a variável running for verdade a janela do pygame vai ficar aberta
 while running:
@@ -16,8 +22,9 @@ while running:
         #se o evento clicado dentro do pygame for o x a tela vai fechar
         if event.type == pygame.QUIT:
             running = False
-
-
+    #isso escolhe a cor do background do pygame
+    screen.fill((255, 0, 0))
+    pygame.display.update()
 
 
 
