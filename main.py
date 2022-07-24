16049c250ecd7ff175f27ac8e2f26237b3828a5a
Denysmak <denysmakene13@gmail.com>
2022-07-24 00:11:35 -0300

    player se move no eixo X

--- main.py
+++ main.py
@@ -19,21 +19,40 @@ playerImg = pygame.image.load('spaceship.png')
 playerImg = pygame.transform.scale(playerImg, (70, 70))
 playerX = 365
 playerY = 480
+playerX_change = 0
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 #GAME LOOP
 running = True
 #enquanto a variável running for verdade a janela do pygame vai ficar aberta
 while running:
+    # isso escolhe a cor do background do pygame
+    screen.fill((0, 0, 0))
     #esse for quer dizer para todos os eventos dentro da tela do pygamer
     for event in pygame.event.get():
         #se o evento clicado dentro do pygame for o x a tela vai fechar
         if event.type == pygame.QUIT:
             running = False
-    #isso escolhe a cor do background do pygame
-    screen.fill((0, 0, 0))
-    player()
+
+    #if keystroke is pressed check whether it is right or left
+        elif event.type == pygame.KEYDOWN:
+            #se pressionarmos o a ou a seta para a esquerda
+            if(event.key == pygame.K_a):
+                playerX_change = -0.3
+
+            elif (event.key == pygame.K_d):
+                playerX_change = 0.3
+
+
+        elif event.type == pygame.KEYUP:
+            if(event.key == pygame.K_a or event.key == pygame.K_d):
+               playerX_change = 0
+
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
 
 
